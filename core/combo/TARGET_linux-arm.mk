84eae393c658ff90ed047d4f13098876f0ab7f27
sub77 <sub77@ymail.com>
2016-06-10 15:15:09 -0500

    Check for Toolchains and set property
    Change-Id: Ib46744d70433bb08dcb3a24d02ece30d8acdc5be

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -35,17 +35,21 @@ TARGET_$(combo_2nd_arch_prefix)ARCH_VARIANT := armv5te
 endif
 
 # Decouple NDK library selection with platform compiler version
-ifeq ($(TARGET_NDK_VERSION_EXP),)
-$(combo_2nd_arch_prefix)TARGET_NDK_GCC_VERSION := 4.9
-else
-$(combo_2nd_arch_prefix)TARGET_NDK_GCC_VERSION := $(TARGET_NDK_VERSION_EXP)
+$(combo_2nd_arch_prefix)TARGET_NDK_VERSION := $(TARGET_NDK_VERSION_EXP)
+ifndef TARGET_NDK_VERSION
+	$(combo_2nd_arch_prefix)TARGET_NDK_VERSION := $(FALLBACK_NDK)
+	ifndef FALLBACK_NDK
+	$(combo_2nd_arch_prefix)TARGET_NDK_VERSION := 4.9
+	endif
 endif
 
-ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
-$(combo_2nd_arch_prefix)TARGET_GCC_VERSION := 4.9
-$(combo_2nd_arch_prefix)TARGET_LEGACY_GCC_VERSION := 4.8
-else
 $(combo_2nd_arch_prefix)TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
+ifndef TARGET_GCC_VERSION
+	$(combo_2nd_arch_prefix)TARGET_GCC_VERSION := $(FALLBACK_GCC)
+	ifndef FALLBACK_GCC
+	$(combo_2nd_arch_prefix)TARGET_GCC_VERSION := 4.9
+	$(combo_2nd_arch_prefix)TARGET_LEGACY_GCC_VERSION := 4.8
+	endif
 endif
 
 TARGET_ARCH_SPECIFIC_MAKEFILE := $(BUILD_COMBOS)/arch/$(TARGET_$(combo_2nd_arch_prefix)ARCH)/$(TARGET_$(combo_2nd_arch_prefix)ARCH_VARIANT).mk
